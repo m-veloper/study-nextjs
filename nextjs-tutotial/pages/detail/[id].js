82f1efee6b8bc340d9aae0d3634662bfa1b473db
m-veloper <m_veloper@naver.com>
2021-11-11 00:29:40 +0900

    정적 생성(Static Generation) - getStaticProps, getStaticPaths

---
+++ nextjs-tutotial/pages/detail/[id].js
@@ -0,0 +1,74 @@
+import Head from "next/head";
+import Axios from "axios";
+import Item from "../../src/component/Item/Item";
+
+/**
+ * 정적페이지
+ * @param item
+ * @param name
+ * @returns {JSX.Element}
+ * @constructor
+ */
+const Post = ({ item, name }) => {
+  return (
+    <>
+      {item && (
+        <>
+          <Head>
+            <title>{item.name}</title>
+            <meta name="description" content={item.description}></meta>
+          </Head>
+          {name} 환경 입니다.
+          <Item item={item} />
+        </>
+      )}
+    </>
+  );
+};
+
+export default Post;
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { id: "740" } },
+      { params: { id: "730" } },
+      { params: { id: "729" } },
+    ],
+    fallback: true,
+  };
+}
+
+export async function getStaticProps(context) {
+  const id = context.params.id;
+  const apiUrl = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
+  const res = await Axios.get(apiUrl);
+  const data = res.data;
+
+  return {
+    props: {
+      item: data,
+      name: process.env.name,
+    },
+  };
+}
+
+/**
+ *
+ * Next js 모든 페이지 사전 렌더링 (Pre-rendering)
+ 더 좋은 퍼포먼스
+ 검색엔진최적화(SEO)
+ 1. 정적 생성
+ 2. Server Side Rendering (SSR, Dynamic Rendering)
+ 차이점은 언제 html 파일을 생성하는가
+ [정적 생성]
+ - 프로젝트가 빌드하는 시점에 html파일들이 생성
+ - 모든 요청에 재사용
+ - 퍼포먼스 이유로, 넥스트 js는 정적 생성을 권고
+ - 정적 생성된 페이지들은 CDN에 캐시
+ - getStaticProps / getStaticPaths
+ [서버사이드 렌더링]은 매 요청마다 html 을 생성
+ - 항상 최신 상태 유지
+ - getServerSideProps
+ */
+
